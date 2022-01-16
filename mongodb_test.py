b3536433720829a67d7f6b8d59951f2e7e89346b
tb12b02 <79048533+tb12b02@users.noreply.github.com>
2022-01-16 11:24:38 -0500

    Delete mongodb_test.py

--- mongodb_test.py
+++
@@ -1,9 +0,0 @@
-import certifi
-from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@cluster0.e5jmi.mongodb.net"
-ca = certifi.where()
-client = MongoClient(url, tlsCAFile=ca)
-db = client.pytech
-print("\n --PyTech Collection List --")
-print(db.list_collection_names())
-input("\n\n End of program.. Press any key to EXIT... ")
